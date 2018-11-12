5622776098cb4377182d3e514df219bd26b898a7
K Flay & St Ven <kasandara.sullivan@accenture.com>
2018-11-12 10:50:56 -0500

    complete struts to spring convert Stephen is greeeaaat\!\!

---
+++ src/main/java/org/superbiz/struts/UsersController.java
@@ -0,0 +1,62 @@
+package org.superbiz.struts;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UsersController {
+    private final Logger logger = LoggerFactory.getLogger(UsersController.class);
+    private final UserRepository userRepository;
+
+    public UsersController (UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/addUser")
+    public String addUserForm() {
+        logger.error("Test");
+        return "addUserForm";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user, Model model) {
+        try {
+            userRepository.save(user);
+        } catch (Exception | Error e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "addUserForm";
+        }
+
+        return "addedUser";
+    }
+
+    @GetMapping("/findUser")
+    public String findUserForm() {
+        return "findUserForm";
+    }
+
+    @PostMapping("/findUser")
+    public String findUser(@RequestParam long id, Model model) {
+        User user = userRepository.findOne(id);
+
+        if (user == null) {
+            model.addAttribute("errorMessage", "User not found");
+            return "findUserForm";
+        }
+
+        model.addAttribute("user", user);
+        return "displayUser";
+    }
+
+    @GetMapping("/list")
+    public String listUsers(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "displayUsers";
+    }
+}
